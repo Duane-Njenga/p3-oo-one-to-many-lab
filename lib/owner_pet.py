762c6675d1c5a57eab508b22462cf66283c6abc0
Dwayne <dune.njenga@student.moringaschool.com>
2025-05-15 17:06:24 +0300

    Completed lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception
+        
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+    
 
+        Pet.all.append(self)
+        if owner and isinstance(owner, Owner):
+            owner.add_pet(self)
+
+    
 class Owner:
-    pass
+    
+    def __init__(self,name):
+        self.name = name
+        self._pets = []
+
+    def pets(self): 
+        return self._pets
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            self._pets.append(pet)
+            pet.owner = self
+        else:
+            raise Exception
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda Pet: Pet.name)
+owner = Owner("Ben")
+pet1 = Pet("Fido", "dog", owner)
+pet2 = Pet("Clifford", "dog", owner)
+print(owner.pets())
